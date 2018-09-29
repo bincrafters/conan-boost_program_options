1301498824cca231158d63f1916f8c77086185ff
Jerry W <jerrywiltse@gmail.com>
2018-09-29 17:15:35 -0400

    move to python_requires strategy

--- conanfile.py
+++ conanfile.py
@@ -1,87 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools
+from conans import python_requires
 
 
-class BoostProgram_OptionsConan(ConanFile):
+base = python_requires("boost_base/1.67.0@bincrafters/testing")
+
+class BoostProgram_OptionsConan(base.BoostBaseConan):
     name = "boost_program_options"
-    version = "1.67.0"
-    author = "Bincrafters <bincrafters@gmail.com>"
-    exports = ["LICENSE.md"]
+    url = "https://github.com/bincrafters/conan-boost_program_options"
     lib_short_names = ["program_options"]
-    is_header_only = False
-
     options = {"shared": [True, False]}
     default_options = "shared=False"
-
     source_only_deps = [
         "bind",
         "tokenizer"
     ]
+    b2_defines = [
+        "BOOST_PROGRAM_OPTIONS_DYN_LINK=1"
+    ]
+    source_only_deps = [
+        "tokenizer",
+    ]
+    b2_requires = [
+        "boost_any",
+        "boost_config",
+        "boost_core",
+        "boost_detail",
+        "boost_function",
+        "boost_iterator",
+        "boost_lexical_cast",
+        "boost_smart_ptr",
+        "boost_static_assert",
+        "boost_throw_exception",
+        "boost_type_traits"
+    ]
 
-    requires = (
-        "boost_any/1.67.0@bincrafters/testing",
-        "boost_config/1.67.0@bincrafters/testing",
-        "boost_core/1.67.0@bincrafters/testing",
-        "boost_detail/1.67.0@bincrafters/testing",
-        "boost_function/1.67.0@bincrafters/testing",
-        "boost_iterator/1.67.0@bincrafters/testing",
-        "boost_lexical_cast/1.67.0@bincrafters/testing",
-        "boost_package_tools/1.67.0@bincrafters/testing",
-        "boost_smart_ptr/1.67.0@bincrafters/testing",
-        "boost_static_assert/1.67.0@bincrafters/testing",
-        "boost_throw_exception/1.67.0@bincrafters/testing",
-        "boost_type_traits/1.67.0@bincrafters/testing"
-    )
 
-    def build_requirements(self):
-        self.build_requires("boost_tokenizer/1.67.0@bincrafters/testing")
-
-    def package_id_additional(self):
-        boost_deps_only = [dep_name for dep_name in self.info.requires.pkg_names if dep_name.startswith("boost_")]
-
-        for dep_name in boost_deps_only:
-            self.info.requires[dep_name].full_version_mode()
-
-    def b2_options(self, lib_name=None):
-        return "define=BOOST_PROGRAM_OPTIONS_DYN_LINK=1"
-
-    # BEGIN
-
-    url = "https://github.com/bincrafters/conan-boost_program_options"
-    description = "Please visit http://www.boost.org/doc/libs/1_67_0"
-    license = "BSL-1.0"
-    short_paths = True
-    generators = "boost"
-    settings = "os", "arch", "compiler", "build_type"
-    build_requires = "boost_generator/1.67.0@bincrafters/testing"
-
-    def package_id(self):
-        getattr(self, "package_id_additional", lambda:None)()
-
-    def source(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.source(self)
-        getattr(self, "source_additional", lambda:None)()
-
-    def build(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.build(self)
-        getattr(self, "build_additional", lambda:None)()
-
-    def package(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package(self)
-        getattr(self, "package_additional", lambda:None)()
-
-    def package_info(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package_info(self)
-        getattr(self, "package_info_additional", lambda:None)()
-
-    # END
